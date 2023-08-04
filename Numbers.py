0343197fa656240e569c04545dfa15a8b31c5dcf
SeaBreeze12 <141295748+SeaBreeze12@users.noreply.github.com>
2023-08-03 21:51:40 -0300

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,59 @@
+# INTEGER
+
+age = 21
+players = 2
+quantity = 5
+
+# FLOAT
+
+print(f"you are {age} years old")
+print(f"there are {players} players online")
+print(f"you have {quantity} apples")
+
+distance = 2.6
+price = 12.90
+
+print(f"you travelled {distance} miles")
+print(f"the price is ${price} dollars")
+
+# STRING
+# String is a serie of characters
+name = "bolo"
+food = "pizza"
+email = "bolodoce12@gmail.com"
+
+print(f"hello {name}")
+print(f"do you like {food}")
+print(f"your email is {email}")
+
+# BOOLEAN
+# True or False
+# Lembrar de não colar os "" ao utilizar as booleans, se não, se tornaria uma string.
+
+online = True
+for_sale = False
+running = False
+
+#print(f"are you online?: {online}")
+#print(f"is the item for sale?: {for_sale}")
+#print(f"game running?: {running}")
+
+if running:
+    print("the game is running")
+else:
+    print("the game is over")
+
+x = 1
+y = 2
+z = 3
+
+print(x)
+print(y)
+print(z)
+    #Ou:
+
+x, y, z = 1, 2 ,3
+
+print(x)
+print(y)
+print(z)
---
+++ Print.py
@@ -0,0 +1,5 @@
+idade = 17
+
+print("Sua idade é " + str(idade) + " anos")
+print("Sua idade é", idade, "anos")
+print(f"sua idade é {idade} anos")
